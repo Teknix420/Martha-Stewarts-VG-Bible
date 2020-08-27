0d26b6f1f0ef93e68f2ba9ba54212e7432137c29
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-08-26 21:01:09 -0500

    added terminology for axios get request.

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,13 @@
 // API Key: 8bf9fa37b1dfeca73818d322443b91fd
-// Request URL: https://api-v3.igdb.com
+// Request URL: https://api-v3.igdb.com\
+// const axios = require('axios');
+ 
+// Make a request for a user with a given ID
+// axios.get('/user?ID=12345')
+// .then(function (response) {
+//   // handle success
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   // handle error
+//   console.log(error);
